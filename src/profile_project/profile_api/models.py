1bc2a85fc89a837a98c73d28858443def8e54023
shabi <shabiali09@gmail.com>
2018-05-17 13:02:27 +0530

    Added aimproved permission profile feed item

--- src/profile_project/profile_api/models.py
+++ src/profile_project/profile_api/models.py
@@ -52,12 +52,13 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
         return self.email
 
 class ProfileFeedItem(models.Model):
-    """Profile staus upadate"""
+    """Profile status update."""
 
-    user_profile = models.ForeignKey('UserProfile',on_delete=models.CASCADE)
-    status_text  = models.CharField(max_length=255)
-    created_on   = models.DateTimeField(auto_now_add=True)
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """return a model as string"""
+        """Return the model as a string."""
+
         return self.status_text
